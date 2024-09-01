c175bc011c1bfae7792fa9252ecadbac837a9a06
kapfereq <145503803+kapfereq@users.noreply.github.com>
2024-09-01 10:39:19 +0200

    final?

---
+++ Utilities/Hurtownia.BusinessLogic/Product.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Hurtownia.BusinessLogic
+{
+    public class Product : INotifyPropertyChanged
+    {
+        private int _quantity;
+        public string Name { get; set; }
+
+        public int Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                if (_quantity != value)
+                {
+                    _quantity = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand RemoveCommand { get; }
+
+        public Product()
+        {
+            AddCommand = new RelayCommand(o => Quantity++, o => true);
+            RemoveCommand = new RelayCommand(o =>
+            {
+                if (Quantity > 0) Quantity--;
+            }, o => Quantity > 0);  // Fixing the lambda expression here
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Utilities/Hurtownia.BusinessLogic/ProductVM.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Hurtownia.BusinessLogic
+{
+    public class ProductVM : INotifyPropertyChanged
+    {
+        public ObservableCollection<Product> Products { get; set; }
+        public ICommand CheckoutCommand { get; }
+
+        public ProductVM()
+        {
+            Products = new ObservableCollection<Product>
+            {
+                new Product { Name = "Product 1", Quantity = 0 },
+                new Product { Name = "Product 2", Quantity = 0 },
+                new Product { Name = "Product 3", Quantity = 0 },
+                new Product { Name = "Product 4", Quantity = 0 },
+                new Product { Name = "Product 5", Quantity = 0 },
+                new Product { Name = "Product 6", Quantity = 0 },
+                new Product { Name = "Product 7", Quantity = 0 },
+                new Product { Name = "Product 8", Quantity = 0 }
+            };
+
+            CheckoutCommand = new RelayCommand(o => Checkout(), o => Products.Count > 0);
+        }
+
+        private void Checkout()
+        {
+            foreach (var product in Products)
+            {
+                product.Quantity = 0;  // Reset quantity for each product
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
